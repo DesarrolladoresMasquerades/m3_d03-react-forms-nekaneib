e8c09880935947879999b4ae9036964d187d753d
Nekane Ibarlucea <nknib90@gmail.com>
2022-03-02 19:32:43 +0100

    fgh

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -9,11 +9,28 @@ function MovieList() {
   const [moviesData, setMoviesData] = useState(moviesDataJSON);
   const [movies, setMovies] = useState(moviesDataJSON);
 
+  function addMovie(newMovie){
+    setMoviesData([...moviesData, newMovie]) 
+    // What if I want to sort or filter before setting the state? ...
+    setMovies([...moviesData, newMovie].sort((a,b)=>a.title > b.title))
+  }
+
+  function filterMovieList(str){
+    let filteredMovies 
+    if (str === "All"){ 
+      filteredMovies = moviesData
+    } else ( 
+      filteredMovies = moviesData.filter((movie)=> {
+        return movie.title[0].toLowerCase() === str.toLowerCase() 
+      })
+    )
+    setMovies(filteredMovies)
+  }
 
   return (
     <div>
       <FilterMovies filterMovies={filterMovieList} />
-      <AddMovie addMovie={addNewMovie} />
+      <AddMovie addMovie={addMovie} />
       {movies.map((movie) => {
         return <MovieCard key={movie._id} movie={movie} />;
       })}
